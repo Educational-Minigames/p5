b5472be225cf9d945a729bb2f826c5606a056673
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-05-07 14:51:12 +0330

    init playground

---
+++ playground/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(100);
+    circle(width / 2, height / 2, width / 2)
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+const ali = 2 + 4;
