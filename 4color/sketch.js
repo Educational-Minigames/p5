d7dff7e729a033f1526dc989974a1df1dfc6cbc7
Mahdi <pmahdicheraghi@gmail.com>
2023-06-03 04:54:20 +0330

    Add more graphs

--- 4color/sketch.js
+++ 4color/sketch.js
@@ -1,58 +1,115 @@
 const COLORS = {
-  RED: "red",
-  BLUE: "blue",
+  YELLOW: "yellow",
   GREEN: "green",
-  // YELLOW: "yellow",
+  BLUE: "blue",
+  RED: "red",
 }
 
-let graph1;
-let graph1States;
+let graphArray = {};
+let stateArray = {};
+let imageArray = {};
+
+let arrow;
+
+let timer;
+
 let state;
+let map;
 let csp;
+
+let pause;
+let play;
 let next;
 let prev;
 let mrv;
 let lcv;
+let sel;
+let div;
+
+function preload() {
+  Object.entries(graph).forEach(([key, value]) => {
+    imageArray[key] = loadImage(`${key}.png`);
+  });
+
+  arrow = loadImage("arrow.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  graph1 = new Graph();
-  graph1.addNode(new Node(100, 100));
-  graph1.addNode(new Node(200, 100));
-  graph1.addNode(new Node(300, 100));
-  graph1.addNode(new Node(100, 200));
-  graph1.addNode(new Node(200, 200));
-  graph1.addNode(new Node(300, 200));
-  graph1.addEdge(0, 1);
-  graph1.addEdge(0, 3);
-  graph1.addEdge(1, 2);
-  graph1.addEdge(1, 4);
-  graph1.addEdge(2, 5);
-  graph1.addEdge(3, 4);
-  graph1.addEdge(4, 5);
-  graph1.addEdge(3, 1);
-  graph1.addEdge(4, 2);
-
+  
   csp = new CSP();
-  graph1States = csp.solve(graph1);
-  state = 0;
+
+  div = createDiv('');
+  div.style('width', '220px');
+  div.style('height', '80px');
+  div.style('background-color', '#00000020');
 
   prev = createButton('prev');
-  prev.position(0, 0);
   prev.mousePressed(prevState);
+  pause = createButton('⏸️');
+  pause.mousePressed(pauseCallback);
+  play = createButton('▶️');
+  play.mousePressed(playCallback);
   next = createButton('next');
-  next.position(60, 0);
   next.mousePressed(nextState);
+
   mrv = createCheckbox('MRV', true);
-  mrv.position(120, 0);
   mrv.changed(mrvChanged);
   lcv = createCheckbox('LCV', true);
-  lcv.position(180, 0);
   lcv.changed(lcvChanged);
+  sel = createSelect();
+  sel.changed(mySelectEvent);
+
+  Object.entries(graph).forEach(([key, value]) => {
+    let g = new Graph();
+    value.points.forEach((p) => {
+      g.addNode(new Node(p[0], p[1]));
+    });
+    value.edges.forEach((e) => {
+      g.addEdge(e[0], e[1]);
+    });
+    graphArray[key] = g;
+    let s = csp.solve(g, mrv.checked(), lcv.checked());
+    stateArray[key] = s;
+    sel.option(key);
+  });
+
+  state = 0;
+  map = Object.entries(graph)[0][0];
+}
+
+function draw() {
+  background(220);
+
+  tint(255, 128);
+  image(imageArray[map], 0, 0, windowWidth, windowHeight);
+
+  stateArray[map][state].draw();
+
+  fill('black');
+  textSize(16);
+  textAlign("center");
+  text(state + 1, windowWidth - 108, 28);
+
+
+
+  prev.position(windowWidth - 206, 10);
+  pause.position(windowWidth - 160, 10);
+  play.position(windowWidth - 95, 10);
+  next.position(windowWidth - 56, 10);
+  mrv.position(windowWidth - 210, 45);
+  lcv.position(windowWidth - 150, 45);
+  sel.position(windowWidth - 80, 40);
+  div.position(windowWidth - 220, 0);
+}
+
+function mySelectEvent() {
+  map = sel.value();
+  state = 0;
 }
 
 function nextState() {
-  if (state < graph1States.length - 1) {
+  if (state < stateArray[map].length - 1) {
     state++;
   }
 }
@@ -64,19 +121,25 @@ function prevState() {
 }
 
 function mrvChanged() {
-  graph1States = csp.solve(graph1, mrv.checked(), lcv.checked());
+  stateArray[map] = csp.solve(graphArray[map], mrv.checked(), lcv.checked());
   state = 0;
 }
 
 function lcvChanged() {
-  graph1States = csp.solve(graph1, mrv.checked(), lcv.checked());
+  stateArray[map] = csp.solve(graphArray[map], mrv.checked(), lcv.checked());
   state = 0;
 }
 
-function draw() {
-  background(220);
-  graph1States[state].draw();
-  text(state + 1, 45, 15);
+function playCallback() {
+  timer = setInterval(() => {
+    if (state < stateArray[map].length - 1) {
+      state++;
+    }
+  }, 500)
+}
+
+function pauseCallback() {
+  clearInterval(timer);
 }
 
 function windowResized() {
@@ -123,22 +186,23 @@ class Graph {
 }
 
 class GraphState {
-  constructor(graph) {
+  constructor(graph, nextNode) {
     this.nodes = graph.nodes.map(node => new Node(node.pos.x, node.pos.y, node.adj));
     this.nodes.forEach((node, i) => node.setColor(graph.nodes[i].color));
     this.domains = this.nodes.map(node => csp.findNodeDomain(node, graph));
+    this.nextNode = nextNode;
   }
 
   draw() {
     this.nodes.forEach((node) => {
       node.adj.forEach(id => {
         const node2 = this.nodes[id];
-        line(node.pos.x, node.pos.y, node2.pos.x, node2.pos.y);
+        line((node.pos.x * windowWidth), (node.pos.y * windowHeight), (node2.pos.x * windowWidth), (node2.pos.y * windowHeight));
       });
     });
     this.nodes.forEach(node => {
       fill(node.color === "" ? 'white' : node.color);
-      circle(node.pos.x, node.pos.y, 30);
+      circle((node.pos.x * windowWidth), (node.pos.y * windowHeight), 30);
     })
     this.domains.forEach((domain, i) => {
       const node = this.nodes[i];
@@ -147,9 +211,13 @@ class GraphState {
       }
       domain.forEach((color, i) => {
         fill(color);
-        circle(node.pos.x + 10 * (i - 1), node.pos.y - 20, 10);
+        circle((node.pos.x * windowWidth) + 10 * (i - 1.5), (node.pos.y * windowHeight) + 20, 10);
       });
     });
+    if (this.nextNode) {
+      noTint();
+      image(arrow, (this.nextNode.pos.x * windowWidth) - 10, (this.nextNode.pos.y * windowHeight) - 45, 20, 30);
+    }
   }
 }
 
@@ -174,13 +242,16 @@ class CSP {
   }
 
   backtrack(graph, mrv, lcv) {
-    this.states.push(new GraphState(graph));
     if (graph.nodes.every(node => node.color !== "")) {
+      this.states.push(new GraphState(graph));
       return true;
     }
     const node = mrv ? this.MRV(graph) : graph.nodes.find(node => node.color === "");
     const domain = lcv ? this.LCV(graph, node) : this.findNodeDomain(node, graph);
+
     for (let i = 0; i < domain.length; i++) {
+      node.removeColor();
+      this.states.push(new GraphState(graph, node));
       node.setColor(domain[i]);
       const result = this.backtrack(graph, mrv, lcv);
 
@@ -190,8 +261,8 @@ class CSP {
     }
     if (domain.length > 0) {
       node.removeColor();
-      this.states.push(new GraphState(graph));
     }
+    this.states.push(new GraphState(graph, node));
     return false;
   }
 
